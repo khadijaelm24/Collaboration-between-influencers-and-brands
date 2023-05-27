63b7b7a17773e038f96ed0eb2d511a12d056ac23
khadijaelm24 <khadijaelmadani24@gmail.com>
2023-05-28 01:20:27 +0200

    insert's php file

---
+++ addpart_inf/insert.php
@@ -0,0 +1,27 @@
+<?php
+include ('database.php');
+session_start();
+
+// Retrieve the user ID from the session
+$user_id = $_SESSION['user_id'];
+$user_type = $_SESSION['user_type'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the form data
+    $marqueId = $_POST['marque'];
+    $duree = $_POST['duree'];
+    $montant = $_POST['montant'];
+    $content = $_POST['content'];
+
+
+    // Insert the data into the "offre" table
+    $sql = "INSERT INTO offre (influencer_id,marque_id, duration,content, price,sender) VALUES ('$user_id','$marqueId','$duree','$content','$duree', '$user_type')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Data inserted successfully!";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+    header("Location: index.php");
+    mysqli_close($conn);
+}
+    ?>
